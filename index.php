abcd77c0f92c3d1b8146e2937660fe380d03f83b
Ebenezer Isaac <ebenezerv99@gmail.com>
2019-11-28 22:05:50 +0530

    relink

--- index.php
+++ index.php
@@ -6,12 +6,13 @@
     </head>
     <body align='center'>
         Hello there!!
-		<br>
-		Major Site Upgrade Underway
         <br>
-		Sorry for the inconvenience
+        Major Site Upgrade Underway
+        <br>
+        Sorry for the inconvenience
         <br>
         <a href="quiz.php">Quiz</a>
-		<a href="quiz.php">About Page</a>
+        <br>
+        <a href="about.php">About Page</a>
     </body>
 </html>
