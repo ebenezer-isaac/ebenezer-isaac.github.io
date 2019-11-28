3845b1bd1b109e08fadfdfd7f0fd65470c4879be
Ebenezer Isaac <ebenezerv99@gmail.com>
2019-11-28 22:32:54 +0530

    index mod

--- index.php
+++ index.php
@@ -1,18 +1,36 @@
-<html>
+<html lang="en">
     <head>
-        <title>
-            Home
-        </title>
+        <title>Home</title>
+        <meta charset="UTF-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1">
+        <link rel="stylesheet" type="text/css" href="css/bootstrap.min.css">
+        <link rel="stylesheet" type="text/css" href="css/util.css">
+        <link rel="stylesheet" type="text/css" href="css/main.css">
     </head>
-    <body align='center'>
-        Hello there!!
-        <br>
-        Major Site Upgrade Underway
-        <br>
-        Sorry for the inconvenience
-        <br>
-        <a href="quiz.php">Quiz</a>
-        <br>
-        <a href="about.php">About Page</a>
+    <body>
+        <div class="limiter">
+            <div class="container-login100">
+                <div id="mained" class="wrap-login100" style='text-align:center'>
+                    <h1>Hello there!!</h1>
+                    <br>
+                    <h2>Links<br> </h2>
+                    <br>
+                    <div style='border:solid 1px black;border-radius: 6px'>
+                        <br>
+                        <a href="quiz.php"> <h2>Quiz </h2></a>
+                        <br>
+                        <a href="about.php"> <h2>About Page </h2></a>
+                        <br>
+                    </div>
+                    <br>
+                    <br>
+                    <h6>Major Site Upgrade Underway<br>Sorry for the inconvenience</h6>
+                </div>
+            </div>
+        </div>
+        <script src="js/bootstrap.min.js" type="168b875311079e67a1884235-text/javascript"></script>
+        <script src="js/main.js" type="168b875311079e67a1884235-text/javascript"></script>
+        <script src="js/jquery-3.4.1.min.js" integrity="sha256-CSXorXvZcTkaix6Yvo6HppcZGetbYMGWSFlBw8HfCJo=" crossorigin="anonymous"></script>
+        <script src="js/bootstrap.min.js" type="168b875311079e67a1884235-text/javascript"></script>
     </body>
 </html>
